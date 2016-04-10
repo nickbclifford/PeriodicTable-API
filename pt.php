251c02f768fe31eb898089c4f60201db37f05483
MiningPotatoes <catcheese12345@gmail.com>
2016-04-10 09:13:14 -0500

    did names functionality

--- pt.php
+++ pt.php
@@ -1,10 +1,28 @@
 <?php
 // api goes here
 // example url: https://nickclifford.me/api/pt.php?mode=names&element=H,Ar,Hf
-
-switch($_GET["mode"]) {
-	case "names":
-		break;
-	default:
-		break;
+if(isset($_GET["mode"])) {
+	switch($_GET["mode"]) {
+		case "names":
+			require('names.php');
+			if(isset($_GET["elements"])) {
+				$elements = explode(',', $_GET["elements"]);
+				foreach ($elements as $index => $symbol) {
+					$elements[$index] = ucfirst(strtolower($symbol));
+				}
+			} else {
+				$elements = array_keys($names);
+			}
+			$result = [];
+			foreach ($elements as $element) {
+				$name = $names[$element];
+				$result[$element] = $name;
+			}
+			echo json_encode($result);
+			break;
+		default:
+			break;
+	}
+} else {
+	// some other stuff
 }
