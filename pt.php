956936a7dd04ea1eb1fa51654fee6de91b162165
Nick Clifford <catcheese12345@gmail.com>
2016-04-10 12:15:13 -0500

    comments and docs

--- pt.php
+++ pt.php
@@ -1,28 +1,34 @@
 <?php
 // api goes here
-// example url: https://nickclifford.me/api/pt.php?mode=names&element=H,Ar,Hf
+// example url: https://nickclifford.me/api/pt.php?mode=names&elements=H,Ar,Hf
 if(isset($_GET["mode"])) {
 	switch($_GET["mode"]) {
 		case "names":
+			// imports the $names array
 			require('names.php');
 			if(isset($_GET["elements"])) {
+				// gets the element symbols from the URL
 				$elements = explode(',', $_GET["elements"]);
 				foreach ($elements as $index => $symbol) {
 					$elements[$index] = ucfirst(strtolower($symbol));
 				}
 			} else {
+				// if none were specified, just do all of them
 				$elements = array_keys($names);
 			}
 			$result = [];
+			// gets the names from $names and puts them in the final array
 			foreach ($elements as $element) {
 				$name = $names[$element];
 				$result[$element] = $name;
 			}
+			// echos the array as JSON
 			echo json_encode($result);
 			break;
 		case "orbitals":
 			break;
 		default:
+			// no modes? echo an error.
 			echo json_encode(["error" => "No mode specified. Please try again!"]);
 			break;
 	}
