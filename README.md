a017cbb190b2de894c449b6a71f0feff9002bbf6
Nick Clifford <nick@nickclifford.me>
2016-12-07 19:53:10 -0600

    add support for multiple modes at a time, updated template and README

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 # Periodic Table API
 This is a RESTful API for the Periodic Table of Elements.
 ## Syntax
-(I'll be using my website, [nickclifford.me](https://nickclifford.me/) as an example address here, with `/api` tacked onto the end.)
+(I'll be using my website, [nickclifford.me](https://nickclifford.me/), as an example address here, with `/api` tacked onto the end.)
 
 The basic syntax is:
-`https://nickclifford.me/api/pt.php?mode={mode}&elements={element symbols}`
+`https://nickclifford.me/api/pt.php?mode={modes}&elements={element symbols}`
 
 Certain options can be added to the GET parameters as well.
 
-The `elements` field contains comma-seperated element symbols. If the `elements` field is removed, **all** elements will be used.
+The `modes` field contains comma-seperated mode values. If the `modes` field is omitted, an error will be returned.
+
+The `elements` field contains comma-seperated element symbols. If the `elements` field is omitted, **all** elements will be used.
 
 ### Modes
 |Valid Modes|Purpose|Options|Purpose|Values|
@@ -18,6 +20,24 @@ The `elements` field contains comma-seperated element symbols. If the `elements`
 |`numbers`|Get the atomic number of an element given it's symbol.|`mass`|Show the atomic mass of the element.|`true`|
 |`dankmemes`|¯\\\_(ツ)_/¯|N/A|N/A|N/A|
 
+### Response
+The JSON response has a top-level `error` field, and a top-level field for each mode you select.
+
+Example:
+```json
+{
+	"error": null,
+	"names": {
+		"Nh": "Nihonium"
+	},
+	"numbers": {
+		"Nh": {
+			"atomic": 133
+		}
+	}
+}
+```
+
 ## Elements
 Now, I know you're saying, *But, Nick, which elements are supported by your wonderful fantastic API?*
 
@@ -26,4 +46,4 @@ The answer? **Every single element on the Periodic Table!** Pretty cool, right?
 ## Issues? Bugs? Compliments?
 If you find a bug or issue, please submit an issue here on [GitHub](https://github.com/MiningPotatoes/PeriodicTable-API).
 
-If you want to help me <del>cry</del> code, feel free to submit a pull request with your code!
+If you want to contribute, feel free to submit a pull request with your code!
--- pt.php
+++ pt.php
@@ -5,134 +5,133 @@ require('names.php');
 require('numbers.php');
 require('electrons.php');
 
+$fullResult = ["error" => null];
+
 if(isset($_GET["mode"])) {
-	switch($_GET["mode"]) {
-		case "names":
-			if(isset($_GET["elements"])) {
-				$errorElements = [];
-				// gets the element symbols from the URL
-				$elements = explode(',', $_GET["elements"]);
-				foreach ($elements as $index => $symbol) {
-					if(in_array(ucfirst(strtolower($symbol)), array_keys($names))) {
-						$elements[$index] = ucfirst(strtolower($symbol));
-					} else {
-						unset($elements[$index]);
-						array_push($errorElements, $symbol);
-					}
-				}
-			} else {
-				// if none were specified, just do all of them
-				$elements = array_keys($names);
-			}
-			$result = [];
-			// gets the names from $names and puts them in the final array
-			foreach ($elements as $element) {
-				$name = $names[$element];
-				$result[$element] = $name;
-			}
-			// pops an error message in the $result
-			if(isset($errorElements)) {
-				foreach ($errorElements as $error) {
-					$result[$error] = ["error" => "Invalid element!"];
-				}
-			}
-			// echos the array as JSON
-			header('Content-Type: application/json');
-			echo json_encode($result);
-			break;
-		case "orbitals":
-		case "electrons":
-			if(isset($_GET["elements"])) {
-				$errorElements = [];
-				// gets the element symbols from the URL
-				$elements = explode(',', $_GET["elements"]);
-				foreach ($elements as $index => $symbol) {
-					if(in_array(ucfirst(strtolower($symbol)), array_keys($electrons))) {
-						$elements[$index] = ucfirst(strtolower($symbol));
-					} else {
-						unset($elements[$index]);
-						array_push($errorElements, $symbol);
-					}
-				}
-			} else {
-				// if none were specified, just do all of them
-				$elements = array_keys($electrons);
-			}
-			$result = [];
-			$atomicNumbers = [];
-			// converts the symbols to atomic numbers (i could probably just use array_push() here, but i don't really care)
-			foreach ($elements as $index => $symbol) {
-				$atomicNumbers[$index] = $numbers[$symbol]["atomic"];
-			}
-			// calculates configurations & gets element blocks if requested
-			foreach ($elements as $index => $element) {
-				$result[$element]["config"] = electron_config($atomicNumbers[$index]);
-				$result[$element]["short"] = electron_config($atomicNumbers[$index], true);
-				if(isset($_GET["showBlocks"])) {
-					if($_GET["showBlocks"]) {
-						$result[$element]["block"] = $electrons[$element]["block"];
-					}
-				}
-			}
-			// pops an error message in the $result
-			if(isset($errorElements)) {
-				foreach ($errorElements as $error) {
-					$result[$error] = ["error" => "Invalid element!"];
-				}
-			}
-			// echos the array as JSON
-			header('Content-Type: application/json');
-			echo json_encode($result);
-			break;
-		case "numbers":
-			if(isset($_GET["elements"])) {
-				$errorElements = [];
-				// gets the element symbols from the URL
-				$elements = explode(',', $_GET["elements"]);
-				foreach ($elements as $index => $symbol) {
-					if(in_array(ucfirst(strtolower($symbol)), array_keys($electrons))) {
-						$elements[$index] = ucfirst(strtolower($symbol));
-					} else {
-						unset($elements[$index]);
-						array_push($errorElements, $symbol);
-					}
-				}
-			} else {
-				// if none were specified, just do all of them
-				$elements = array_keys($numbers);
-			}
-			$result = [];
-			// gets atomic numbers and possibly masses from $numbers, passes them to $resultsp
-			foreach ($elements as $element) {
-				$result[$element]["atomic"] = $numbers[$element]["atomic"];
-				if(isset($_GET["mass"])) {
-					if($_GET["mass"]) {
-						$result[$element]["mass"] = $numbers[$element]["mass"];
-					}
-				}
-			}
-			// pops an error message in the $result
-			if(isset($errorElements)) {
-				foreach ($errorElements as $error) {
-					$result[$error] = ["error" => "Invalid element!"];
-				}
-			}
-			// echos the array as JSON
-			header('Content-Type: application/json');
-			echo json_encode($result);
-			break;
-		case "dankmemes":
-			header('Content-Type: application/json');
-			echo json_encode([69 => "( ͡° ͜ʖ ͡°)"]);
-			break;
-		default:
-			// invalid mode? echo an error.
-			header('Content-Type: application/json');
-			echo json_encode(["error" => "Invalid mode. Please try again!"]);
-			break;
-	}
+    foreach(explode(',', $_GET["mode"]) as $mode) {
+        switch($mode) {
+        case "names":
+            if(isset($_GET["elements"])) {
+                $errorElements = [];
+                // gets the element symbols from the URL
+                $elements = explode(',', $_GET["elements"]);
+                foreach ($elements as $index => $symbol) {
+                    if(in_array(ucfirst(strtolower($symbol)), array_keys($names))) {
+                        $elements[$index] = ucfirst(strtolower($symbol));
+                    } else {
+                        unset($elements[$index]);
+                        array_push($errorElements, $symbol);
+                    }
+                }
+            } else {
+                // if none were specified, just do all of them
+                $elements = array_keys($names);
+            }
+            $result = [];
+            // gets the names from $names and puts them in the final array
+            foreach ($elements as $element) {
+                $name = $names[$element];
+                $result[$element] = $name;
+            }
+            // pops an error message in the $result
+            if(isset($errorElements)) {
+                foreach ($errorElements as $error) {
+                    $result[$error] = "Invalid element!";
+                }
+            }
+            $fullResult[$mode] = $result;
+            break;
+        case "orbitals":
+        case "electrons":
+            if(isset($_GET["elements"])) {
+                $errorElements = [];
+                // gets the element symbols from the URL
+                $elements = explode(',', $_GET["elements"]);
+                foreach ($elements as $index => $symbol) {
+                    if(in_array(ucfirst(strtolower($symbol)), array_keys($electrons))) {
+                        $elements[$index] = ucfirst(strtolower($symbol));
+                    } else {
+                        unset($elements[$index]);
+                        array_push($errorElements, $symbol);
+                    }
+                }
+            } else {
+                // if none were specified, just do all of them
+                $elements = array_keys($electrons);
+            }
+            $result = [];
+            $atomicNumbers = [];
+            // converts the symbols to atomic numbers (i could probably just use array_push() here, but i don't really care)
+            foreach ($elements as $index => $symbol) {
+                $atomicNumbers[$index] = $numbers[$symbol]["atomic"];
+            }
+            // calculates configurations & gets element blocks if requested
+            foreach ($elements as $index => $element) {
+                $result[$element]["config"] = electron_config($atomicNumbers[$index]);
+                $result[$element]["short"] = electron_config($atomicNumbers[$index], true);
+                if(isset($_GET["showBlocks"])) {
+                    if($_GET["showBlocks"]) {
+                        $result[$element]["block"] = $electrons[$element]["block"];
+                    }
+                }
+            }
+            // pops an error message in the $result
+            if(isset($errorElements)) {
+                foreach ($errorElements as $error) {
+                    $result[$error] = "Invalid element!";
+                }
+            }
+            $fullResult[$mode] = $result;
+            break;
+        case "numbers":
+            if(isset($_GET["elements"])) {
+                $errorElements = [];
+                // gets the element symbols from the URL
+                $elements = explode(',', $_GET["elements"]);
+                foreach ($elements as $index => $symbol) {
+                    if(in_array(ucfirst(strtolower($symbol)), array_keys($electrons))) {
+                        $elements[$index] = ucfirst(strtolower($symbol));
+                    } else {
+                        unset($elements[$index]);
+                        array_push($errorElements, $symbol);
+                    }
+                }
+            } else {
+                // if none were specified, just do all of them
+                $elements = array_keys($numbers);
+            }
+            $result = [];
+            // gets atomic numbers and possibly masses from $numbers, passes them to $resultsp
+            foreach ($elements as $element) {
+                $result[$element]["atomic"] = $numbers[$element]["atomic"];
+                if(isset($_GET["mass"])) {
+                    if($_GET["mass"]) {
+                        $result[$element]["mass"] = $numbers[$element]["mass"];
+                    }
+                }
+            }
+            // pops an error message in the $result
+            if(isset($errorElements)) {
+                foreach ($errorElements as $error) {
+                    $result[$error] = "Invalid element!";
+                }
+            }
+            $fullResult[$mode] = $result;
+            break;
+        case "dankmemes":
+            $fullResult[$mode] = [69 => "( ͡° ͜ʖ ͡°)"];
+            break;
+        default:
+            // invalid mode? echo an error.
+            $fullResult["error"] = "Invalid mode. Please try again!";
+            break;
+        }
+    }       
 } else {
-	// no modes? echo an error.]
-	header('Content-Type: application/json');
-	echo json_encode(["error" => "No mode specified. Please try again!"]);
+	// no modes? echo an error.
+	$fullResult["error"] = "No mode specified. Please try again!";
 }
+
+// echo everything that's been set.
+header('Content-Type: application/json');
+echo json_encode($fullResult);
